4aad2da1318e21ad4ae3c4c508cc09e3a1b43586
Sebastian Salas <sebastian.salas@team-work.cl>
2024-10-08 00:49:58 -0300

    feat: image reloads in canvas are resolved and data types, primary key, and non-null and null values are incorporated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { ReactElement, useEffect, useRef, useState } from "react";
 import Styles from "../src/assets/main.module.css";
+import EntitiesJSON from "../src/json/entities.json";
 
 interface IEntityPropertiesRows {
   name: string;
+  type: string;
   pk?: boolean;
+  nn?: boolean;
 }
 
 interface IEntityProperty {
@@ -30,95 +33,98 @@ interface IDrawEntityBase {
   properties: IEntityProperty;
   color: string;
   name: string;
+  align: CanvasTextAlign;
+  type: string;
   pk?: boolean;
+  nn?: boolean;
 }
 
-type IDrawEntityTable = Omit<IDrawEntityBase, "color" | "pk">;
+type IDrawEntityTable = Omit<IDrawEntityBase, "color" | "pk" | "align" | "type">;
 type IDrawEntityHeader = Omit<
   IDrawEntityBase,
-  "properties" | "color" | "name" | "img" | "pk"
+  "properties" | "color" | "name" | "img" | "pk" | "align" | "type"
 >;
 type IDrawEntityProperty = Omit<
   IDrawEntityBase,
-  "properties" | "name" | "img" | "pk"
+  "properties" | "name" | "img" | "pk" | "align" | "type"
 >;
-type IDrawEntityTextHeader = Omit<
+type IDrawEntityText = Omit<
   IDrawEntityBase,
-  "properties" | "color" | "img" | "pk"
->;
-type IDrawEntityTextProperty = Omit<
-  IDrawEntityBase,
-  "properties" | "color" | "img"
+  "properties" | "img"
 >;
 
 interface ISvgImages {
-  tableIcon: string;
+  [tableIcon: string]: string;
   primaryKeyIcon: string;
+};
+
+interface IDrawSvg {
+  ctx: CanvasRenderingContext2D;
+  image: HTMLImageElement;
+  x: number;
+  y: number;
+  height: number;
+  widthImage: number;
+  heightImage: number;
+};
+
+interface IFontFamilyPrompts {
+  fontSize: string;
+  fontWeight?: number;
+};
+
+interface IFontFamily {
+  [textProperty: string]: (prompts: IFontFamilyPrompts) => string;
+  typeProperty: (prompts: IFontFamilyPrompts) => string;
 }
 
 function Index(): ReactElement<HTMLDivElement> {
   const refCanvas = useRef<HTMLCanvasElement>(null);
-  const [entities] = useState<IEntityTable[]>([
-    {
-      width: 300,
-      x: 100,
-      y: 50,
-      height: 50,
-      name: "productos",
-      properties: {
-        height: 40,
-        rows: [
-          {
-            name: "id",
-            pk: true,
-          },
-          {
-            name: "name",
-          },
-          {
-            name: "enable",
-          },
-          {
-            name: "FechaNacimiento",
-          },
-          {
-            name: "Texto",
-          },
-          {
-            name: "Example",
-          },
-        ],
-      },
-    },
-    {
-      width: 250,
-      x: 600,
-      y: 300,
-      height: 38,
-      name: "Pais",
-      properties: {
-        height: 38,
-        rows: [
-          {
-            name: "id",
-          },
-        ],
-      },
-    },
-  ]);
-  const [svgImages, setSvgImages] = useState<ISvgImages>({
+  const [entities] = useState<IEntityTable[]>(EntitiesJSON);
+  const [svgImages] = useState<ISvgImages>({
     tableIcon: "/tableIcon.svg",
     primaryKeyIcon: "/primaryKeyIcon.svg",
+    nullableIcon: "/nullableIcon.svg",
+    notNullableIcon: "/notNullableIcon.svg",
+  });
+  const [zoom, setZoom] = useState<number>(1);
+  const [loadedImages, setLoadedImages] = useState<{
+    [key: string]: HTMLImageElement;
+  }>({});
+  const [fontFamily] = useState<IFontFamily>({
+    textProperty: (prompts: IFontFamilyPrompts) =>
+      `${prompts.fontWeight} ${prompts.fontSize} Inter var,ui-sans-serif,system-ui,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emojif`,
+    typeProperty: (prompts: IFontFamilyPrompts) =>
+      `${prompts.fontSize} consolas,monospace`,
   });
 
   useEffect(() => {
-    draw();
-    window.addEventListener("resize", draw);
+    if (Object.keys(loadedImages).length === Object.keys(svgImages).length) {
+      draw();
+      window.addEventListener("resize", draw);
 
-    return () => {
-      window.removeEventListener("resize", draw);
+      return () => {
+        window.removeEventListener("resize", draw);
+      };
+    }
+  }, [loadedImages, zoom]);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const images: { [key: string]: HTMLImageElement } = {};
+      for (const key in svgImages) {
+        const img = new Image();
+        img.src = svgImages[key];
+        await new Promise((resolve) => {
+          img.onload = resolve;
+        });
+        images[key] = img;
+      }
+      setLoadedImages(images);
     };
-  }, []);
+
+    loadImages();
+  }, [svgImages]);
 
   const draw = (): void => {
     const canvas = refCanvas.current;
@@ -131,11 +137,20 @@ function Index(): ReactElement<HTMLDivElement> {
       const ctx = canvas.getContext("2d");
 
       if (ctx) {
+        ctx.scale(dpr * zoom, dpr * zoom);
         drawBackground(ctx, canvas.width, canvas.height);
         entities.forEach((entity) => {
           const { x, y, width, height, properties, name } = entity;
           drawEntityTable({ ctx, x, y, width, height, properties, name });
-          drawSVGLoad(ctx, x, y, height, svgImages.tableIcon);
+          drawSVG({
+            ctx,
+            x: x + 14,
+            y: y + height / 2 - 13,
+            height,
+            image: loadedImages.tableIcon,
+            widthImage: 25,
+            heightImage: 25,
+          });
         });
       }
     }
@@ -156,11 +171,24 @@ function Index(): ReactElement<HTMLDivElement> {
     const { ctx, x, y, width, height, properties, name } = prompts;
     const { rows, height: heightProperty } = properties;
     drawHeaderTable({ ctx, x, y, width, height });
-    drawEntityTextHeader({ ctx, x, y, width, height, name });
+    drawEntityText({
+      ctx,
+      x: x + 5,
+      y,
+      width,
+      height,
+      name,
+      pk: false,
+      nn: false,
+      align: "left",
+      type: "titleTable",
+      color: "white",
+    });
 
     rows.forEach((row: IEntityPropertiesRows, index: number) => {
-      const { name, pk } = row;
+      const { name, type, pk, nn } = row;
       const propertyY: number = y + height + heightProperty * index;
+      let addPositionXText: number = 0;
 
       drawEntityProperty({
         ctx,
@@ -171,7 +199,7 @@ function Index(): ReactElement<HTMLDivElement> {
         color: "rgb(50, 50, 50)",
       });
 
-      drawEntityTextProperty({
+      drawEntityText({
         ctx,
         x,
         y: propertyY,
@@ -179,18 +207,49 @@ function Index(): ReactElement<HTMLDivElement> {
         height: heightProperty,
         name,
         pk,
+        nn,
+        align: "left",
+        type: "textProperty",
+        color: "white",
       });
 
-      if (pk)
-        drawSVGLoad(
+      drawEntityText({
+        ctx,
+        x,
+        y: propertyY,
+        width,
+        height: heightProperty,
+        name: type,
+        pk,
+        nn,
+        align: "right",
+        type: "typeProperty",
+        color: "rgb(200, 200, 200)",
+      });
+
+      if (pk) {
+        addPositionXText += 22;
+
+        drawSVG({
           ctx,
-          svgImages.primaryKeyIcon,
-          x,
-          propertyY,
+          image: loadedImages.primaryKeyIcon,
+          x: x + 16,
+          y: propertyY + 8,
           height,
-          20,
-          20
-        );
+          widthImage: 20,
+          heightImage: 20,
+        });
+      }
+
+      drawSVG({
+        ctx,
+        image: nn ? loadedImages.notNullableIcon : loadedImages.nullableIcon,
+        x: x + addPositionXText + 17,
+        y: propertyY + 10,
+        height,
+        widthImage: 18,
+        heightImage: 18,
+      });
 
       if (index > 0) {
         drawEntityLineProperty({
@@ -238,8 +297,8 @@ function Index(): ReactElement<HTMLDivElement> {
     ctx.stroke();
   };
 
-  const drawEntityTextHeader = (prompts: IDrawEntityTextHeader): void => {
-    const { ctx, y, x, width, height, name } = prompts;
+  const drawEntityTextHeader = (prompts: IDrawEntityText): void => {
+    const { ctx, y, x, height, name } = prompts;
     ctx.beginPath();
     ctx.font =
       "500 15px Inter var,ui-sans-serif,system-ui,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emojif";
@@ -247,45 +306,57 @@ function Index(): ReactElement<HTMLDivElement> {
     ctx.fillText(name, x + 45, y + height / 2 + 5);
   };
 
-  const drawEntityTextProperty = (prompts: IDrawEntityTextProperty): void => {
-    const { ctx, y, x, width, height, name, pk } = prompts;
+  const drawEntityText = (prompts: IDrawEntityText): void => {
+    const { ctx, y, x, height, width, name, pk, align, type, color } = prompts;
     ctx.beginPath();
-    ctx.font = `${
-      !pk ? "300" : "500"
-    } 15px Inter var,ui-sans-serif,system-ui,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emojif`;
-    ctx.fillStyle = "white";
-    const positionX: number = !pk ? x + 20 : x + 50;
-    ctx.fillText(name, positionX, y + height / 2 + 5);
-  };
-
-  const drawSVG = (
-    ctx: CanvasRenderingContext2D,
-    img: HTMLImageElement | null,
-    x: number,
-    y: number,
-    height: number,
-    widthImage: number,
-    heightImage: number
-  ): void => {
-    if (img) {
-      ctx.drawImage(img, x + 15, y + height / 2 - 13, widthImage, heightImage);
+    if (type === "textProperty") {
+      ctx.font = fontFamily.textProperty({
+        fontSize: "15px",
+        fontWeight: !pk ? 300 : 500,
+      });
     }
+
+    if (type === "typeProperty") {
+      ctx.font = fontFamily.typeProperty({
+        fontSize: "11px",
+      });
+    }
+
+    if(type === "titleTable"){
+      ctx.font = fontFamily.textProperty({
+        fontSize: "15px",
+        fontWeight: 500,
+      });
+    }
+
+    const textMetrics = ctx.measureText(name);
+    const textWidth = textMetrics.width;
+
+
+    ctx.fillStyle = color;
+    ctx.textAlign = align;
+
+    let addPositionX: number = 20;
+
+    if (pk) {
+      addPositionX += 22;
+    }
+
+    addPositionX += 22;
+
+    let textX: number = x + addPositionX;
+
+    if(type === "typeProperty"){
+      textX = x + width - 15;
+    }
+
+    ctx.fillText(name, textX, y + height / 2 + 5);
   };
 
-  const drawSVGLoad = (
-    ctx: CanvasRenderingContext2D,
-    image: string,
-    x: number,
-    y: number,
-    height: number,
-    widthImage: number,
-    heightImage: number
-  ): void => {
-    const img = new Image();
-    img.src = image;
-    img.onload = () => {
-      drawSVG(ctx, img, x, y, height, widthImage, heightImage);
-    };
+  const drawSVG = (prompts: IDrawSvg): void => {
+    const { ctx, image, x, y, height, widthImage, heightImage } = prompts;
+      
+    ctx.drawImage(image, x, y, widthImage, heightImage);
   };
 
   return (
